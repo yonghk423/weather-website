fc5fef51c16e0e27cfe83279598f8ba9c26a12be
yonghk423 <yonghk423423@gmail.com>
2022-12-06 16:16:16 +0900

    destructuring assignment api data

--- src/api/WeatherApi.ts
+++ src/api/WeatherApi.ts
@@ -7,11 +7,17 @@ const WeatherApi = async (city: string) => {
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
       )
-      .then((data) => data);
+      .then((data) => data?.data);
     console.log(response);
-    return response;
+    const { weather } = response;
+    const { main, icon } = weather[0];
+    const {
+      main: { temp, temp_min, temp_max },
+      name,
+    } = response;
+
+    return { main, icon, temp, temp_min, temp_max, name };
   } catch (err) {
-    console.log(err);
     throw new Error("API Request Error:, error ");
   }
 };
